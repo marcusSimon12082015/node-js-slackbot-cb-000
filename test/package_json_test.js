755b654a257aa9698f6b20e6a174160b7592a918
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-08 12:02:41 +0300

    Add package_json tests

---
+++ test/package_json_test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect;
+const packageJson = require('../package.json');
+
+describe('package.json', () => {
+  it('has the correct dependencies', () => {
+   expect(Object.keys(packageJson.dependencies)).to.include.members([
+     'express',
+   ]);
+  });
+
+  it('has the correct devDependencies', () => {
+   expect(Object.keys(packageJson.devDependencies)).to.include.members([
+     'chai',
+     'mocha',
+     'mocha-multi',
+     'supertest'
+   ]);
+  });
+});
