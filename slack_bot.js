ec9ec0db49ca88dc5c5ce059ed16864a95272c77
Ethan Zane Miller <ethanzanemiller@gmail.com>
2016-05-08 12:03:52 +0300

    Add initial slackbot server stub

---
+++ slack_bot.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+exports.listen = function(port, callback) {
+  callback = (typeof callback != 'undefined') ? callback : () => {
+    console.log('Listening on ' + port + '...');
+  };
+  app.listen(port, callback);
+};
